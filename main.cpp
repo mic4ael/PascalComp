e333837ef8659b21d9284e85e6f4674e20a08b87
mic4ael <crossner90@gmail.com>
2016-12-27 17:51:34 +0100

    Clean up after parsing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include "parser.hpp"
+#include "codegenerator.hpp"
+#include "symboltable.hpp"
+#include "symbol.hpp"
 
 extern int yyparse();
+extern CodeGenerator *codeGenerator;
+extern SymbolTable *symbolTable;
 
 int main()
 {
-    return yyparse();
+    int parseResult = yyparse();
+
+    cout << symbolTable->tableStr() << endl;
+
+    delete codeGenerator;
+    delete symbolTable;
+
+    return parseResult;
 }
